254cc41eefe3a8e6ade72c6532ef57a3c3beda07
Rachel Tao <rt1726@nyu.edu>
2019-05-11 20:13:08 -0400

    sudoku done

--- Project2/sudoku.py
+++ Project2/sudoku.py
@@ -2,9 +2,10 @@ import math;
 import copy;
 
 class state:
+	#class state has two attributes
 	def __init__(self, lst, domain):
-		self.lst = lst;
-		self.domain = domain;
+		self.lst = lst;          #lst contains the current sudoku
+		self.domain = domain;	 #domain contains the current domains for all cells
 
 
 	#check if there is any early failure
@@ -19,18 +20,15 @@ class state:
 			temp = set();
 			for col in range(9):
 
-				#print(self.lst[row][col], self.lst[row][col] == "0")
 				if self.lst[row][col] != "0":
 					if self.lst[row][col] in temp:
 						print("1111111111")
 						return False;
 					temp.add(self.lst[row][col]);
-			#print(temp);
 			for col in range(9):
 				if self.lst[row][col] != "0":
 					self.domain[row * 9 + col] = set();
 				else:
-					#print(self.domain[row * 9 + col])
 					self.domain[row * 9 + col] -= temp;
 					if len(self.domain[row * 9 + col]) == 0:
 						print("2222222222", row, col)
@@ -71,7 +69,6 @@ class state:
 							return False;
 						if self.lst[row][col] != "0":
 							temp.add(self.lst[row][col]);
-				#print(mul1, mul2, temp);
 				for row in range(3 * mul2 - 3, 3 * mul2):
 					for col in range(3 * mul1 - 3, 3 * mul1):
 						if self.lst[row][col] != "0":
@@ -98,7 +95,13 @@ class state:
 				break;
 			index += 1;
 		if setVal == 1:
-			self.forwardChecking();
+			res = self.forwardChecking();
+			if type(res) == list:
+				return res;
+		print("finishing forwardChecking...")
+		if self.found() == True:
+			#print("returning result")
+			return self.lst;
 		return True;
 
 
@@ -156,6 +159,7 @@ class state:
 
 		return lowMRV[indexCell]
 
+	#Check if the result is found
 	def found(self):
 		for i in self.lst:
 			for j in i:
@@ -164,15 +168,18 @@ class state:
 		return True;
 
 
-
+	#the backtraking algorithm
 	def nextState(self):
 		i = self.nextCell()[0];
 		j = self.nextCell()[1];
 		domain = self.domain[i * 9 + j];
 		print(i, j, domain)
 		state_temp = copy.deepcopy(self);
-		if state_temp.forwardChecking == False:
-
+		res = state_temp.forwardChecking();
+		if type(res) == list:
+			print("NextState returning result...")
+			return res;
+		if res == False:
 			return False;
 		self.domain = copy.deepcopy(state_temp.domain);
 		self.lst = copy.deepcopy(state_temp.lst);
@@ -183,7 +190,11 @@ class state:
 			
 			
 			state_temp = copy.deepcopy(self);
-			if state_temp.forwardChecking() == False:
+			res = state_temp.forwardChecking();
+			if type(res) == list:
+				print("NextState returning result...")
+				return res;
+			if res == False:
 				print("forwardChecking failed.")
 				self.lst[i][j] = "0";
 			else:
@@ -193,8 +204,12 @@ class state:
 					setForm(state_temp.lst);
 					return state_temp.lst;
 				temp_nextState = state_temp.nextState();
-				
-				if type(temp_nextState) == 'list':
+
+					
+				if type(temp_nextState) == list:
+					print(setForm(temp_nextState))
+					print("returning lst")
+					print()
 					return temp_nextState;
 
 			print(i, j, "Keep guessing");
@@ -210,34 +225,40 @@ class state:
 
 
 		
-
+	#main search function
+	#catch the return data from nextState
 	def search(self):
-
-		if self.forwardChecking() == False:
-			print("first forwardChecking")
+		res = self.forwardChecking()
+		if res == False:
+			#print("first forwardChecking")
 			return None;
+		elif type(res) == list:
+			print(res)
+			return res;
 		for i in self.domain:
 			print(i)
 		
-		if self.nextState() == True:
-			return self.lst;
+		res = self.nextState();
+		if type(res) == list:
+			return res;
 		else:
 			return None;
 
 		
 
 		
-			
+#print the sudoku in 9 x 9 
 def setForm(lst):
 	for i in lst:
 		print(" ".join(i));
 
 
-
+#read file
 def readin():
 	sudoku_lst = [];
 	print("Please enter the sudoku file name: ");
-	with open("test2.txt") as file:
+	filename = input();
+	with open(filename) as file:
 		for i in range(9):
 			content = file.readline();
 			content = content.split(" ");
@@ -246,6 +267,7 @@ def readin():
 				content.remove("\n");
 			sudoku_lst.append(content);
 	#setForm(sudoku_lst);
+	file.close();
 	return sudoku_lst;
 
 
@@ -261,9 +283,15 @@ def main():
 	state_new = state(sudoku_lst, domain);
 	#print(state_new.forwardChecking());
 	#print(state_new.nextCell());
-	print(state_new.search());
-	#print(state_new.domain);
-	#setForm(state_new.lst);
+	res = state_new.search();
+	f = open("Output", "w+");
+	if res == None:
+		f.write(None);
+	else:
+		for i in res:
+			f.write(" ".join(i));
+			f.write("\n");
+	f.close();
 
 main();
 
